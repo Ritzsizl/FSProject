6d6052136b895d3bc57c912f3455efde4cb90f53
Ritzsizl <ritzraha4u@gmail.com>
2024-12-16 19:28:33 +0530

    Hero Page small Window vid dev

--- aniproject/src/App.jsx
+++ aniproject/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Hero from './components/Hero'
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <main className='relative min-h-screen w-screen overflow-x-hidden'>
+      <Hero/>
+    </main>
   )
 }
 
